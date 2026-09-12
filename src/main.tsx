e29e7b43fa6daaf27dc027d40f120c965f24edfe
svksharma <301975153+SVKSharma@users.noreply.github.com>
2026-09-12 16:34:38 +0600

    fixed toastify stack gap & use FIFO structure

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ createRoot(document.getElementById("root")!).render(
       position="bottom-right"
       autoClose={2400}
       hideProgressBar={false}
-      newestOnTop={false}
+      newestOnTop={true}
       closeOnClick={false}
       rtl={false}
       pauseOnFocusLoss
@@ -20,7 +20,7 @@ createRoot(document.getElementById("root")!).render(
       theme="light"
       transition={Slide}
       className="z-9999 bottom-3! left-6! right-2! w-auto! sm:bottom-4! sm:left-auto! sm:right-4!"
-      toastClassName="mx-2"
+      toastClassName="mx-2 mb-2!"
     />
   </StrictMode>,
 );
